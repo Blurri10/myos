7f9c01916152f63a3f4381bd8080003c7660c0ca
Blurri10 <mbasilnore.10@gmail.com>
2024-02-28 01:49:14 +0530

    added js file and modified code

--- app.js
+++ app.js
@@ -2,6 +2,13 @@ const timer = setInterval(function () {
     const today = new Date().getTime();
     const dueDate = new Date('2024-03-01T00:00:00').getTime();
     const diff = dueDate - today;
-    document.querySelector('#test').innerText = `${Math.floor((((diff / 1000) / 60) / 60) % 24)}`;
-}, 10);
+
+    const seconds = diff / 1000;
+    const minutes = seconds / 60;
+    const hours = minutes / 60;
+    const days = hours / 24;
+
+    document.querySelector('#date').innerText = `${Math.floor(days)}`;
+    document.querySelector('#seconds').innerText = `.${days.toFixed(6).substring(2)}`;
+}, 70);
 
