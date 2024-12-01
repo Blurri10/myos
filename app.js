3199b273c61a8bad90c7bb606d5a68422c868e6e
Basil <120660471+Blurri10@users.noreply.github.com>
2024-12-02 01:39:44 +0530

    fixed month text

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const months = {
-    '01': 'january',
-    '02': 'february',
-    '03': 'march',
-    '04': 'april',
-    '05': 'may',
-    '06': 'june',
-    '07': 'july',
-    '08': 'august',
-    '09': 'september',
-    '10': 'october',
-    '11': 'november',
-    '12': 'december',
+    0: 'january',
+    1: 'february',
+    2: 'march',
+    3: 'april',
+    4: 'may',
+    5: 'june',
+    6: 'july',
+    7: 'august',
+    8: 'september',
+    9: 'october',
+    10: 'november',
+    11: 'december',
 }
 
 setInterval(function () {
@@ -40,5 +40,5 @@ setInterval(function () {
 
     document.querySelector('#date').innerText = `${Math.floor(days)}`;
     document.querySelector('#seconds').innerText = `${days.toFixed(6).substring(idx)}`;
-    document.querySelector('#month').innerText = months[nextMonth];
+    document.querySelector('#month').innerText = months[currentMonth];
 }, 70);
